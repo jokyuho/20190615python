7e65e1456dd3cb213c1851be28218a83ac1f3f43
jokyuho <guho64@naver.com>
2019-06-29 14:34:34 +0900

    2019.06.29 고객관리 완성

--- python_basic/customer/customer_result.py
+++ python_basic/customer/customer_result.py
@@ -3,39 +3,7 @@ import re
 class Customer:
     custlist=[]
     page = -1
-
-    def firstinput(self):
-        choice=input('''
-            다음 중에서 하실 일을 골라주세요 :
-            I - 고객 정보 입력
-            C - 현재 고객 정보 조회
-            P - 이전 고객 정보 조회
-            N - 다음 고객 정보 조회
-            U - 고객 정보 수정
-            D - 고객 정보 삭제
-            Q - 프로그램 종료
-            ''').upper()  
-        return choice
-
-    def exe(self, choice):
-            if choice=='I':
-                self.insertData()
-            elif choice=='C':
-                self.curSearch()
-            elif choice=='P':
-                self.preSearch()
-            elif choice=='N':
-                self.nextSearch()
-            elif choice=='U':
-                self.updateData()
-            elif choice=='D':
-                self.deleteData()
-            elif choice=='Q':
-                quit()
-
-    def __init__(self):
-        while True:
-            self.exe(self.firstinput())
+    
     
     def insertData(self): 
         customer={'name':'','sex':'',"email":'',"birthyear":''}
@@ -78,8 +46,8 @@ class Customer:
         self.custlist.append(customer)
         print(self.custlist)
         self.page = len(self.custlist)-1 
-        print(self.page)
-
+        print(self.page)   
+        
     def curSearch(self):
         if self.page >= 0:
             print("현재 페이지는 {}쪽 입니다".format(self.page + 1)) 
@@ -105,7 +73,26 @@ class Customer:
             print("현재 페이지는 {}쪽 입니다".format(self.page + 1))
             print(self.custlist[self.page])
 
-    def updateData(self):
+    def deleteData(self):
+        choice1 = input('삭제하려는 고객 정보의 이메일을 입력하세요.')
+        delok = 0
+        for i in range(0,len(self.custlist)):
+            while self.custlist[i]['email'] == choice1:
+                print('{} 고객님의 정보가 삭제되었습니다.'.format(self.custlist[i]['name']))
+                del self.custlist[i]
+                print(self.custlist)
+                self.page=len(self.custlist)-1   
+                print(self.page)  
+                delok = 1
+                break
+            
+            if delok == 1:
+                break
+
+        if delok == 0:
+                print('등록되지 않은 이메일입니다.')
+
+    def updateData(self): 
         while True:
             choice1=input('수정하시려는 고객 정보의 이메일을 입력하세요 : ')
             idx=-1
@@ -129,24 +116,37 @@ class Customer:
             else:
                 print('존재하지 않는 정보입니다.')
                 break
+    def firstinput(self):
+        choice=input('''
+            다음 중에서 하실 일을 골라주세요 :
+            I - 고객 정보 입력
+            C - 현재 고객 정보 조회
+            P - 이전 고객 정보 조회
+            N - 다음 고객 정보 조회
+            U - 고객 정보 수정
+            D - 고객 정보 삭제
+            Q - 프로그램 종료
+            ''').upper()  
+        return choice
 
-    def deleteData(self):
-        choice1 = input('삭제하려는 고객 정보의 이메일을 입력하세요.')
-        delok = 0
-        for i in range(0,len(self.custlist)):
-            while self.custlist[i]['email'] == choice1:
-                print('{} 고객님의 정보가 삭제되었습니다.'.format(self.custlist[i]['name']))
-                del self.custlist[i]
-                print(self.custlist)
-                self.page=len(self.custlist)-1   
-                print(self.page)  
-                delok = 1
-                break
-            
-            if delok == 1:
-                break
+    def exe(self,choice):
+            if choice=='I':
+                self.insertData()
+            elif choice=='C':
+                self.curSearch()
+            elif choice=='P':
+                self.preSearch()
+            elif choice=='N':
+                self.nextSearch()
+            elif choice=='U':
+                self.updateData()
+            elif choice=='D':
+                self.deleteData()
+            elif choice=='Q':
+                quit()
 
-        if delok == 0:
-                print('등록되지 않은 이메일입니다.')
-
+    def __init__(self):
+        while True:
+            self.exe(self.firstinput())
+                
 Customer()
