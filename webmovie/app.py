f755ccc796cb1b05048f632c74557b51b18a6ca4
jokyuho <guho64@naver.com>
2019-07-26 00:11:42 +0900

    숙제 완성!

--- webmovie/app.py
+++ webmovie/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import db, urllib.request, comment
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -6,9 +8,65 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
-@app.route('/secondpage.html')
-def second():
-      return render_template('secondpage.html')
+@app.route('/recommend')
+def recommend():
+    return render_template('recommend.html')
+
+@app.route('/insertform')
+def insertform():
+    return render_template('customer.html')
+
+@app.route('/insert', methods=['POST'])
+def insert_user():
+    user = request.form.to_dict()
+    print(list(user.values()))
+    db.insert_users(list(user.values()))
+    return redirect('/list')
+
+@app.route('/list')
+def list_user():
+    users=db.all_users()
+    return render_template('list.html',users=users)
+
+@app.route('/content/<userid>')
+def content_user(userid):
+    user=db.one_user(userid)
+    return render_template('content.html',user=user)
+
+@app.route('/delete/<userid>')
+def delete_user(userid):
+    db.delete_user(userid)
+    return redirect('/list')
+
+@app.route('/updateform/<userid>')
+def updateform(userid):
+    user=db.one_user(userid)
+    return render_template('updateform.html',user=user)
+
+@app.route('/update',methods=['POST'])
+def update_user():
+    user = request.form.to_dict()
+    print(user)
+    db.update_user(user)
+    return redirect('/list')
+
+@app.route('/secondpage1.html')
+def second1():
+      return render_template('secondpage1.html')
+
+@app.route('/secondpage2.html')
+def second2():
+      return render_template('secondpage2.html')
+
+@app.route('/secondpage3.html')
+def second3():
+      return render_template('secondpage3.html')
+
+@app.route('/usercomment')
+def list_usercomment():
+    comment.commentmade()
+    items=db.users_comment()
+    return render_template('usercomment.html',items=items)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- webmovie/db.py
+++ webmovie/db.py
@@ -14,7 +14,7 @@ def conn_db():
 def create_table():
     conn=conn_db()
     cursor=conn.cursor()
-    #my_books테이블 생성(제목,출판일자,출판사,페이지수,추천여부)
+    #customer 테이블 생성(아이디,이메일,주소,비밀번호,고유값)
     cursor.execute('''create table if not exists users(
         userid varchar(11) NOT NULL, 
         email varchar(255)  NOT NULL,
@@ -76,5 +76,44 @@ def update_user(user):
             where userid=%s
         '''
     cursor.execute(sql,(user['email'],user['address'],user['password'],user['userid']))
+    conn.commit()
+    conn.close()
+
+
+#테이블 생성 함수
+def create_table2():
+    conn=conn_db()
+    cursor=conn.cursor()
+    # movie테이블 생성(글번호,평점,영화제목,140자평,날짜)
+    # (no integer, grade integer, title text, content text, writer text, date text)
+    cursor.execute('''create table if not exists movie(
+        no int NOT NULL, 
+        grade int,
+        title varchar(255),
+        content varchar(300),
+        writer varchar(30),
+        date varchar(20)
+        )''')
+    conn.commit()
+    conn.close()
+
+#전체 데이터 출력 함수
+def users_comment():
+    conn=conn_db()
+    cursor=conn.cursor()
+    cursor.execute("select * from movie")
+    items=cursor.fetchall()
+    print(len(items)) #레코드 개수 출력
+    print(items)
+    conn.close()
+    return items
+
+
+#데이터 입력 함수
+def insert_movie(items):
+    conn=conn_db()
+    cursor=conn.cursor()
+    sql='INSERT INTO movie VALUES (%s, %s, %s, %s, %s, %s)'
+    cursor.executemany(sql,items)
     conn.commit()
     conn.close()
